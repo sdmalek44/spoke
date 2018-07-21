0124732ea53e3143b8f9fb5c8baaccf484f0bcd4
Stephen Malek <malek.stephen44@gmail.com>
2018-07-21 15:19:29 -0600

    make a path to dashboard after create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to dashboard_index_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
   get '/login', to: 'sessions#new'
-  resources :users, only: [:new]
+  resources :users, only: [:new, :create]
   resources :stations, only: [:show, :index]
   resources :conditions, only: [:index, :show]
   resources :trips, only: [:show]
+  resources :dashboard, only: [:index]
 end
