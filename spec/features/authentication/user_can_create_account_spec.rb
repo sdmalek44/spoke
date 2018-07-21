dbdc1e60b904d2ea93dab407b7d7a888b3dadedf
Stephen Malek <malek.stephen44@gmail.com>
2018-07-21 15:03:55 -0600

    add users table test for create new user

---
+++ spec/features/authentication/user_can_create_account_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'when user visits /login page' do
+  it 'can create a new account' do
+    visit login_path
+
+    click_link "Create Account"
+
+    expect(current_path).to eq(new_user_path)
+    username = 'happyharry'
+
+    fill_in :user_email, with: 'email@email.email'
+    fill_in :user_username, with: username
+    fill_in :user_password, with: 'turtles'
+
+    click_button "Create Account"
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("Logged in as #{username}")
+    expect(page).to have_link("Logout")
+    expect(page).to_not have_link("Login")
+  end
+end
