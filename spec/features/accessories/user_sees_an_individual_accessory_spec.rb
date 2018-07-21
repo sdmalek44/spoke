beb8dc4313064514e198e6e547ec5c0ebbf1718d
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-21 16:04:21 -0600

    Wrote test for retired accessory show page. WIP

--- spec/features/accessories/user_sees_an_individual_accessory_spec.rb
+++ spec/features/accessories/user_sees_an_individual_accessory_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "User visits '/accessory/:id'" do
+describe "A visitor visits '/accessory/:id'" do
   it 'they see the image, title, description, and price for that accessory' do
     accessory_1 = Accessory.create(title: 'Horn', description: 'Let people know you are on a bike', price: 12.50)
 
@@ -10,5 +10,16 @@ describe "User visits '/accessory/:id'" do
     expect(page).to have_content(accessory_1.description)
     expect(page).to have_content("$12.50")
     expect(page).to have_link('Add to Cart')
+  end
+
+  it "they see 'Accessory Retired' instead of 'Add to Cart' if the accessory is retired" do
+    accessory_1 = Accessory.create(title: 'Horn', description: 'Let people know you are on a bike', price: 12.50)
+
+    visit accessory_path(accessory_1)
+
+    expect(page).to have_content(accessory_1.title)
+    expect(page).to have_content(accessory_1.description)
+    expect(page).to have_content("$12.50")
+    expect(page).to have_link('')
   end
 end
