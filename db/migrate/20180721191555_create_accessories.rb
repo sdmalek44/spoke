3ee3221d0d255398c51f9283bb733a90a03bd210
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-21 14:26:10 -0600

    Add default url to accesory image.

--- db/migrate/20180721191555_create_accessories.rb
+++ db/migrate/20180721191555_create_accessories.rb
@@ -1,7 +1,7 @@
 class CreateAccessories < ActiveRecord::Migration[5.1]
   def change
     create_table :accessories do |t|
-      t.string :image
+      t.string :image, default: 'https://asp24.ru/images/companies/1/toughswitch_poe/pics6_1.jpg'
       t.string :title
       t.text :description
       t.integer :price
---
+++ spec/features/accessories/user_sees_an_individual_accessory_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe "User visits '/accessory/:id'" do
+  it 'they see the image, title, description, and price for that accessory' do
+  
+  end
+end
