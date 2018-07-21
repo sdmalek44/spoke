0874826b8e2af67e9a70a801f11557858865004e
Stephen Malek <malek.stephen44@gmail.com>
2018-07-20 19:20:09 -0600

    add comments to station and trip models

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -8,6 +8,10 @@ class Trip < ApplicationRecord
   validates_presence_of :zip_code
 
   belongs_to :bike
+
+  #this creates a connection of these 2 foreign keys to the station id in the station table
+  #each start and end station id has one associated station
+  #see station model
   belongs_to :station, class_name: 'Station', foreign_key: :start_station_id
   belongs_to :station, class_name: 'Station', foreign_key: :end_station_id
 end
