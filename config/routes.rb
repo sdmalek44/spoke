73826c58ff73cff896b662628e05545b49d0e836
Stephen Malek <malek.stephen44@gmail.com>
2018-07-21 14:21:22 -0600

    create root and link to login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "welcome#index"
+  get '/login', to: 'sessions#new'
   resources :stations, only: [:show, :index]
   resources :conditions, only: [:index, :show]
   resources :trips, only: [:show]
