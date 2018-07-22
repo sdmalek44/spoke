e9cad7a07ce2410a4e43da9a444db267e490c797
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-22 11:37:15 -0600

    Add trip data and seed logic.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'CSV'
+
+station_data = CSV.open('./db/csv/station.csv', headers: true, header_converters: :symbol)
+station_data.each do |station|
+  Station.create!(
+    id: station[:id],
+    name: station[:name],
+    dock_count: station[:dock_count],
+    city: station[:city],
+    installation_date: Date.strptime(station[:installation_date], "%m/%d/%Y")
+  )
+end
+
+puts "Seeded #{Station.count} stations."
+puts "Installation Date: #{Station.last.installation_date}"
