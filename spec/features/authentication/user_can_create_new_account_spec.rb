04f9e636302ea5055b65987bf29225276d3ec229
Stephen Malek <malek.stephen44@gmail.com>
2018-07-21 18:17:11 -0600

    add test for if user doesnt enter all attributes for create user

--- spec/features/authentication/user_can_create_new_account_spec.rb
+++ spec/features/authentication/user_can_create_new_account_spec.rb
@@ -17,4 +17,18 @@ describe 'when user visits /login page' do
 
     expect(current_path).to eq(dashboard_index_path)
   end
+  it 'renders new user view if incorrect input' do
+
+        visit new_user_path
+
+        username = 'happyharry'
+
+        fill_in :user_email, with: 'email@email.email'
+        fill_in :user_username, with: ''
+        fill_in :user_password, with: 'turtles'
+
+        click_button "Create Account"
+
+        expect(current_path).to eq(users_path)
+  end
 end
