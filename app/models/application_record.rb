cef51ec4b48c9fa40b794c0dc4d4ac6e5dcf2b17
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-21 18:06:18 -0600

    Fix typo in search method for application record.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,11 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def self.search(term, page)
-    if term
-      where('name LIKE ?', "%#{term}%").paginate(page: page, per_page: 30).order('id DESC')
-    else
-      paginate(page: page, per_page: 30).order('id DESC')
-    end
+  def self.search(page)
+    paginate(page: page, per_page: 30).order('id DESC')
   end
 end
