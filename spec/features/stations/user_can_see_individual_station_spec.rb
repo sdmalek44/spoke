b9e79447f39558ded2d241ef4834b2907a6a3565
Stephen Malek <malek.stephen44@gmail.com>
2018-07-24 14:23:31 -0600

    refactor installation date to print as strftime

--- spec/features/stations/user_can_see_individual_station_spec.rb
+++ spec/features/stations/user_can_see_individual_station_spec.rb
@@ -10,7 +10,7 @@ describe 'user can go to station show page' do
     expect(page).to have_content(station.name)
     expect(page).to have_content(station.dock_count)
     expect(page).to have_content(station.city)
-    expect(page).to have_content(station.installation_date)
+    expect(page).to have_content(station.installation_date.strftime("%m/%d/%Y"))
   end
   it 'can see the number of rides started at this station' do
     station = Station.create!(name: '2name', dock_count: 45, city: 'city', installation_date: Date.new(2017, 3, 10))
