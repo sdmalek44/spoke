a57957f1e9aa0bfedbf9845712f0673b5a67e008
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-07-24 15:50:06 -0600

    adds method to get all dates with a max temp in a range of max temps

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,5 +1,5 @@
 class Condition < ApplicationRecord
-  validates :date, presence: true, uniqueness: true 
+  validates :date, presence: true, uniqueness: true
   validates_presence_of :max_temperature
   validates_presence_of :mean_temperature
   validates_presence_of :min_temperature
@@ -7,4 +7,8 @@ class Condition < ApplicationRecord
   validates_presence_of :mean_visibility
   validates_presence_of :mean_wind_speed
   validates_presence_of :precipitation
+
+  def self.dates_within_max_temp_range(temperature)
+    where("max_temperature between #{temperature} and #{temperature + 9}").pluck(:date)
+  end
 end
--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -11,4 +11,26 @@ describe Condition, type: :model do
     it {should validate_presence_of :mean_wind_speed}
     it {should validate_presence_of :precipitation}
   end
+  describe 'class methods' do
+    it "return dates based on range of max_temperatures" do
+      @condition_1 = Condition.create(date: Date.new(2017, 12, 11), max_temperature: 75.0, mean_temperature: 65.0, min_temperature: 55.0, mean_humidity: 75.0, mean_visibility: 10.0, mean_wind_speed: 11.0, precipitation: 0.23)
+      @condition_2 = Condition.create(date: Date.new(2016, 11, 10), max_temperature: 70.0, mean_temperature: 60.0, min_temperature: 50.0, mean_humidity: 65.0, mean_visibility: 5.0, mean_wind_speed: 12.0, precipitation: 0.12)
+      @condition_3 = Condition.create(date: Date.new(2016, 1, 1), max_temperature: 81.0, mean_temperature: 66.0, min_temperature: 51.0, mean_humidity: 65.0, mean_visibility: 5.0, mean_wind_speed: 22.0, precipitation: 1.12)
+      @condition_4 = Condition.create(date: Date.new(2016, 5, 5), max_temperature: 83.0, mean_temperature: 68.0, min_temperature: 53.0, mean_humidity: 65.0, mean_visibility: 5.0, mean_wind_speed: 33.0, precipitation: 2.12)
+
+      temperature = 70
+
+      dates = Condition.dates_within_max_temp_range(temperature)
+      expected_dates = [Date.new(2017, 12, 11), Date.new(2016, 11, 10)]
+
+      expect(dates).to eq(expected_dates)
+
+      temperature = 80
+
+      dates = Condition.dates_within_max_temp_range(temperature)
+      expected_dates = [Date.new(2016, 1, 1), Date.new(2016, 5, 5)]
+
+      expect(dates).to eq(expected_dates)
+    end
+  end
 end
