ba28e8a9e97e9645eaa98bf73bc31e03ab9fc12d
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-24 16:56:11 -0600

    Add 'admin_user?' helper method to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def admin_user?
+    current_user && current_user.admin?
+  end
+
   def set_cart
     @cart ||= Cart.new(session[:cart])
   end
