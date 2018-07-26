a0fb8dd2fabdbe107858c31c172773477e41544d
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-26 14:57:17 -0600

    Validate users can view bike shop and see all accessories.

---
+++ spec/features/accessories/user_can_see_all_accessories_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "A user visits '/bike-shop'" do
+  describe 'as a visitor' do
+    it 'they see all accessories and their information' do
+      accessory_1 = Accessory.create(title: 'Horn', description: 'Let people know you are on a bike', price: 12.50)
+      accessory_2 = Accessory.create(title: 'Water Bottle', description: 'Stay hydrated', price: 8.50)
+
+      visit bike_shop_path
+
+      expect(page).to have_content(accessory_1.title)
+      expect(page).to have_content(accessory_1.description)
+      expect(page).to have_content("Price: $#{accessory_1.price}")
+      expect(page).to have_content(accessory_2.title)
+      expect(page).to have_content(accessory_2.description)
+      expect(page).to have_content("Price: $#{accessory_2.price}")
+    end
+  end
+end
