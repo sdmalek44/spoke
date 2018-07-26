44c9572d9289d896a0169abb85dd8814d526d51c
Stephen Malek <malek.stephen44@gmail.com>
2018-07-26 17:34:15 -0600

    write more tests back to 100% coverage

--- spec/features/users/admin_can_visit_dashboard_spec.rb
+++ spec/features/users/admin_can_visit_dashboard_spec.rb
@@ -8,7 +8,7 @@ describe 'when admin user visits /dashboard' do
     @accessory2 = Accessory.create!(title: 'title2', description: 'desc2', price: 20.00)
     @accessory3 = Accessory.create!(title: 'title3', description: 'desc3', price: 30.00)
 
-    @order_1 = @user.orders.create!(status: 'Completed')
+    @order_1 = @user.orders.create!(status: 'ordered')
     @order_accessory1 = @order_1.order_accessories.create!(quantity: 5, accessory_id: @accessory1.id)
     @order_accessory2 = @order_1.order_accessories.create!(quantity: 3, accessory_id: @accessory2.id)
 
@@ -16,9 +16,11 @@ describe 'when admin user visits /dashboard' do
     @order_2.order_accessories.create!(quantity: 2, accessory_id: @accessory2.id)
     @order_2.order_accessories.create!(quantity: 3, accessory_id: @accessory3.id)
 
-    @order_3 = @user.orders.create!(status: 'canceled')
+    @order_3 = @user.orders.create!(status: 'paid')
     @order_3.order_accessories.create!(quantity: 4, accessory_id: @accessory1.id)
     @order_3.order_accessories.create!(quantity: 4, accessory_id: @accessory3.id)
+
+    @order_4 = @user.orders.create!(status: 'cancelled')
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
   end
   it 'can see list of all orders' do
@@ -27,5 +29,62 @@ describe 'when admin user visits /dashboard' do
     expect(page).to have_content("Order Id: #{@order_1.id}")
     expect(page).to have_content("Order Id: #{@order_2.id}")
     expect(page).to have_content("Order Id: #{@order_3.id}")
+  end
+  it 'can select all orders' do
+    visit admin_dashboard_path
+
+    click_on "Sort By Status"
+    click_on "All"
+
+    expect(page).to have_content("Order Id: #{@order_1.id}")
+    expect(page).to have_content("Order Id: #{@order_2.id}")
+    expect(page).to have_content("Order Id: #{@order_3.id}")
+    expect(page).to have_content("Order Id: #{@order_4.id}")
+  end
+  it 'can select all orders' do
+    visit admin_dashboard_path
+
+    click_on "Sort By Status"
+    click_on "Ordered"
+
+    expect(page).to have_content("Order Id: #{@order_1.id}")
+    expect(page).to_not have_content("Order Id: #{@order_2.id}")
+    expect(page).to_not have_content("Order Id: #{@order_3.id}")
+    expect(page).to_not have_content("Order Id: #{@order_4.id}")
+  end
+  it 'can select paid orders' do
+    visit admin_dashboard_path
+
+    click_on "Sort By Status"
+    click_on "Paid"
+
+    expect(page).to_not have_content("Order Id: #{@order_1.id}")
+    expect(page).to_not have_content("Order Id: #{@order_2.id}")
+    expect(page).to have_content("Order Id: #{@order_3.id}")
+    expect(page).to_not have_content("Order Id: #{@order_4.id}")
+
+  end
+  it 'can select all orders' do
+    visit admin_dashboard_path
+
+    click_on "Sort By Status"
+    click_on "Cancelled"
+
+    expect(page).to_not have_content("Order Id: #{@order_1.id}")
+    expect(page).to_not have_content("Order Id: #{@order_2.id}")
+    expect(page).to_not have_content("Order Id: #{@order_3.id}")
+    expect(page).to have_content("Order Id: #{@order_4.id}")
+  end
+  it 'can select all orders' do
+    visit admin_dashboard_path
+
+    click_on "Sort By Status"
+    click_on "Completed"
+
+    expect(page).to_not have_content("Order Id: #{@order_1.id}")
+    expect(page).to_not have_content("Order Id: #{@order_3.id}")
+    expect(page).to have_content("Order Id: #{@order_2.id}")
+    expect(page).to_not have_content("Order Id: #{@order_4.id}")
+
   end
 end
