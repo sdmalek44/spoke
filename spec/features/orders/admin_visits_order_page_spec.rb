e0e2b7f37febe4941af3d8df8c852b49de9e419e
Stephen Malek <malek.stephen44@gmail.com>
2018-07-26 19:54:42 -0600

    add code for admin order show

--- spec/features/orders/admin_visits_order_page_spec.rb
+++ spec/features/orders/admin_visits_order_page_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 describe 'when registered user visits /dashboard' do
   before(:each) do
-    @user = User.create!(username: 'happyharry', email: 'email@email.email', password: 'turtles', first_name: 'hare', last_name: 'margret', address: '1234 dolly st.')
+    @admin = User.create!(username: 'happyharry', email: 'email@email.email', password: 'turtles', first_name: 'hare', last_name: 'margret', address: '1234 dolly st.', role: 1)
     @accessory1 = Accessory.create!(title: 'title1', description: 'desc1', price: 10.00)
     @accessory2 = Accessory.create!(title: 'title2', description: 'desc2', price: 20.00)
     @accessory3 = Accessory.create!(title: 'title3', description: 'desc3', price: 30.00)
 
-    @order_1 = @user.orders.create!(status: 'Completed')
+    @order_1 = @admin.orders.create!(status: 'Completed')
     @order_accessory1 = @order_1.order_accessories.create!(quantity: 5, accessory_id: @accessory1.id)
     @order_accessory2 = @order_1.order_accessories.create!(quantity: 3, accessory_id: @accessory2.id)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
   end
   it 'can see an orders attributes' do
     visit order_path(@order_1)
