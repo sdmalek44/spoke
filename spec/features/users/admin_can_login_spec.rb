e29cb47ae568dc6d590ed0f62bab59707a206291
Stephen Malek <malek.stephen44@gmail.com>
2018-07-28 15:05:31 -0600

    update test

--- spec/features/users/admin_can_login_spec.rb
+++ spec/features/users/admin_can_login_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'when admin user visits /' do
   before(:each) do
     @user = User.create!(username: 'happyharry', email: 'email@email.email', password: 'turtles', first_name: 'hare', last_name: 'margret', address: '1234 dolly st.')
-    @admin = User.create!(username: 'bob', email: 'bob@email.email', password: 'bob', role: 1, first_name: 'billy', last_name: 'bobthorton', address: '5555 billybob st')
+    @admin = User.create!(username: 'bob', email: 'bob@email.email', password: 'bob', role: 1, first_name: 'bob', last_name: 'bobthorton', address: '5555 billybob st')
   end
   it 'can login as admin and see dashboard' do
     visit root_path
