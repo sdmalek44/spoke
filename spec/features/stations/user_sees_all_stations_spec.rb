254d22e34a8444e6a54eef1a3f4284a6715fd3b7
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-28 21:13:08 -0600

    Fix tests that broke because of styling changes.

--- spec/features/stations/user_sees_all_stations_spec.rb
+++ spec/features/stations/user_sees_all_stations_spec.rb
@@ -9,16 +9,16 @@ describe "A visitor visits '/stations'" do
     visit stations_path
 
     expect(page).to have_content("#{station_1.name}")
-    expect(page).to have_content("Dock Count: #{station_1.dock_count}")
-    expect(page).to have_content("City: #{station_1.city}")
-    expect(page).to have_content("Installation Date: #{station_1.installation_date.strftime('%m/%d/%Y')}")
+    expect(page).to have_content("#{station_1.dock_count}")
+    expect(page).to have_content("#{station_1.city}")
+    expect(page).to have_content("#{station_1.installation_date.strftime('%m/%d/%Y')}")
     expect(page).to have_content("#{station_2.name}")
-    expect(page).to have_content("Dock Count: #{station_2.dock_count}")
-    expect(page).to have_content("City: #{station_2.city}")
-    expect(page).to have_content("Installation Date: #{station_2.installation_date.strftime('%m/%d/%Y')}")
+    expect(page).to have_content("#{station_2.dock_count}")
+    expect(page).to have_content("#{station_2.city}")
+    expect(page).to have_content("#{station_2.installation_date.strftime('%m/%d/%Y')}")
     expect(page).to have_content("#{station_3.name}")
-    expect(page).to have_content("Dock Count: #{station_3.dock_count}")
-    expect(page).to have_content("City: #{station_3.city}")
-    expect(page).to have_content("Installation Date: #{station_3.installation_date.strftime('%m/%d/%Y')}")
+    expect(page).to have_content("#{station_3.dock_count}")
+    expect(page).to have_content("#{station_3.city}")
+    expect(page).to have_content("#{station_3.installation_date.strftime('%m/%d/%Y')}")
   end
 end
