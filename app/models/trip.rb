1dd7b3017fc120e23b60b5b568b87fb1aa9e75b8
Colin Armstrong <colinwarmstrong@gmail.com>
2018-07-31 14:25:27 -0600

    Refactor dashboard method in trips controllers; also refactors methods within the trip model.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -13,34 +13,16 @@ class Trip < ApplicationRecord
   belongs_to :start_station, class_name: 'Station', foreign_key: :start_station_id, dependent: :destroy
   belongs_to :end_station, class_name: 'Station', foreign_key: :end_station_id, dependent: :destroy
 
-  def self.average_ride_duration
-    average(:duration)
+  def self.ride_durations
+    [maximum(:duration), average(:duration), minimum(:duration)]
   end
 
-  def self.longest_ride_duration
-    maximum(:duration)
-  end
-
-  def self.shortest_ride_duration
-    minimum(:duration)
-  end
-
-  def self.most_frequent_start_station
-    station = select('start_station_id, COUNT(start_station_id) AS start_station_count')
+  def self.station_info
+    stations = select('start_station_id, COUNT(start_station_id) AS start_station_count')
     .group(:start_station_id)
     .order('start_station_count DESC')
-    .limit(1)
-    .first
-    Station.find(station.start_station_id)
-  end
-
-  def self.most_frequent_end_station
-    station_id = select('end_station_id, COUNT(end_station_id) AS end_station_count')
-    .group(:end_station_id)
-    .order('end_station_count DESC')
-    .limit(1)
-    .first
-    Station.find(station_id.end_station_id)
+    {most_frequent_start_station: Station.find(stations.first.start_station_id),
+     most_frequent_end_station: Station.find(stations.last.start_station_id)}
   end
 
   def self.rides_per_month
@@ -51,37 +33,25 @@ class Trip < ApplicationRecord
     group("date_trunc('year', start_date)").order('count_all DESC').count
   end
 
-  def self.most_ridden_bike
-    most_ridden_bike = select('bike_id, COUNT(bike_id) AS ride_count')
+  def self.bike_info
+    ordered_bikes = select('bike_id, COUNT(bike_id) AS ride_count')
     .group(:bike_id)
     .order('ride_count DESC')
     .limit(1)
     .first
-    {most_ridden_bike.bike_id => most_ridden_bike.ride_count}
+    {most_ridden_bike: [ordered_bikes.first.id, ordered_bikes.first.ride_count],
+    least_ridden_bike: [ordered_bikes.last.id, odered_bikes.last.id, ordered_bikes.last.ride_count]}
   end
 
-  def self.least_ridden_bike
-    least_ridden_bike = select('bike_id, COUNT(bike_id) AS ride_count')
-    .group(:bike_id)
-    .order('ride_count ASC')
-    .limit(1)
-    .first
-    {least_ridden_bike.bike_id => least_ridden_bike.ride_count}
+  def self.subscription_type_info
+    [group(:subscription_type).count, Trip.count]
   end
 
-  def self.subscription_type_count
-    group(:subscription_type).count
-  end
-
-  def self.date_with_most_rides
-    date_rides = group(:start_date).order('count_all DESC').count.first
-    condition = Condition.find_by(date: date_rides.first)
-    {date: date_rides.first, rides: date_rides.last, condition: condition}
-  end
-
-  def self.date_with_least_rides
-    date_rides = group(:start_date).order('count_all ASC').count.first
-    condition = Condition.find_by(date: date_rides.first)
-    {date: date_rides.first, rides: date_rides.last, condition: condition}
+  def self.date_info
+    date_rides = group(:start_date).order('count_all DESC').count
+    condition_1 = Condition.find_by(date: date_rides.first.first)
+    condition_2 = Condition.find_by(date: date_rides.last.first)
+    {most_rides: [date: date_rides.first.first, rides: date_rides.first.last, condition: condition_1],
+     least_rides: [date: date_rides.last.first, rides: date_rides.last.last, condition: condition_2]}
   end
 end
